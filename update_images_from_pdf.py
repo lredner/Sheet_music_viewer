6cf559ff0c6a6c58b18546e317ad14fbc7920f1e
Lewis Redner <lewis.redner@colorado.edu>
2023-10-02 20:43:43 -0700

    Decent progress lol

--- update_images_from_pdf.py
+++ update_images_from_pdf.py
@@ -1,33 +1,34 @@
 import pdf2image
 import os
-import threading
+from PIL import Image
+import numpy as np
+import copy
 
-def convert_individual_pdf(source_pdf, target_image_folder, piece_name):
-    pdf2image.convert_from_path(source_pdf, output_folder = target_image_folder, output_file = piece_name, fmt = 'png')
+def convert_new_pdf_to_image(source_path, target_path, pdf):
 
-def convert_new_pdf_to_image(source_path, target_path):
-    threads = []
+    piece_name = pdf.split('.')[0]
 
-    # iterate over contents of source for pdfs and convert if not converted
-    for pdf in os.listdir(source_path):
-        # ignore if not a pdf, or if the images folder exists and has content
-        if not pdf.endswith('.pdf'):
-            continue
-        piece_name = pdf.split('.')[0]
+    target_image_folder = os.path.join(target_path, piece_name)
+    os.mkdir(target_image_folder)
 
-        target_image_folder = os.path.join(target_path, piece_name)
-        if os.path.exists(target_image_folder):
-            if len(os.listdir(target_image_folder)) > 0:
-                continue
-        else:
-            os.mkdir(target_image_folder)
-
-        source_pdf = os.path.join(source_path, pdf)
-
-        x = threading.Thread(target=convert_individual_pdf, args=(source_pdf,target_image_folder,piece_name,))
-        x.start()
-        threads.append(x)
-    
-    for index, thread in enumerate(threads):
-        thread.join()
+    source_pdf = os.path.join(source_path, pdf)
+    pdf2image.convert_from_path(source_pdf, output_folder = target_image_folder, output_file = piece_name, fmt = 'png', thread_count = 4)
         
+
+def change_colour_background(target_image_folder):
+    for image in os.listdir(target_image_folder):
+        im = Image.open(os.path.join(target_image_folder, image))
+        im_matrix = np.array(im, dtype = np.int32)
+        updated_matrix = copy.deepcopy(im_matrix)
+        updated_matrix[:,:,0] = (0.0393*im_matrix[:,:,0] + 0.0769*im_matrix[:,:,1] + 0.0189*im_matrix[:,:,2])*7.5
+        updated_matrix[:,:,1] = (0.0349*im_matrix[:,:,0] + 0.0686*im_matrix[:,:,1] + 0.0168*im_matrix[:,:,2])*7.5
+        updated_matrix[:,:,2] = (0.0272*im_matrix[:,:,0] + 0.0534*im_matrix[:,:,1] + 0.0131*im_matrix[:,:,2])*7.5
+        updated_matrix[updated_matrix>255] = 255
+        updated_image = Image.fromarray(updated_matrix.astype(np.uint8))
+        updated_image.save(os.path.join(target_image_folder, image))
+        # tr = 0.393R + 0.769G + 0.189B
+        # tg = 0.349R + 0.686G + 0.168B
+        # tb = 0.272R + 0.534G + 0.131B
+
+if __name__ == "__main__":
+   quit()
